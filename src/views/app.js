4578e486e0fb2685fb77a0eb6a9e2aec68c815e0
Be1en <valeria.godoy@tecsup.edu.pe>
2023-12-12 21:52:00 -0500

    12/12/2023x2

--- src/views/app.js
+++ src/views/app.js
@@ -3,44 +3,13 @@ import HeadNav from '../components/HeadNav';
 import FloatingMenu from '../components/FloatingMenu';
 import Card from '../components/card';
 
-const cardData = [
-  {
-    image: 'URL_DE_LA_IMAGEN_1',
-    mission: 'Mall Porongoche - AQP',
-    placeName: 'ver',
-  },
-  {
-    image: 'URL_DE_LA_IMAGEN_2',
-    mission: 'Mall del sur - LIMA',
-    placeName: 'ver',
-  },
-  {
-    image: 'URL_DE_LA_IMAGEN_2',
-    mission: 'Parque Lambramani - AQP',
-    placeName: 'ver',
-  },
-  {
-    image: 'URL_DE_LA_IMAGEN_2',
-    mission: 'Real Plaza - AQP',
-    placeName: 'ver details',
-  },
-  // Agrega más datos de tarjetas según sea necesario
-];
-
 function App() {
   return (
     <div className="App">
       <HeadNav />
       <FloatingMenu />
       <div className="cards-container">
-        {cardData.map((card, index) => (
-          <Card
-            key={index}
-            image={card.image}
-            mission={card.mission}
-            placeName={card.placeName}
-          />
-        ))}
+        
       </div>
     </div>
   );
